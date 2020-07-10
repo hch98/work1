a5be1061b326b5430e7800c30a57062cc0be96a2
hch98 <1602373539@qq.com>
2020-07-10 11:27:58 +0800

    ssss

---
+++ dem/src/main/java/com/test.java
@@ -0,0 +1,11 @@
+package com;
+
+import com.tt.my;
+
+public class test {
+    public static void main(String[] args) {
+        my m=new my();
+        m.setId(1);
+        System.out.println(m.getId());
+    }
+}
